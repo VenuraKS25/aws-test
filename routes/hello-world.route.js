c91360f4c5ac400c734344b4a2f270a099090b5b
VenuraKS25 <vks250102@gmail.com>
2024-06-21 15:21:27 +0530

    Minor change

--- routes/hello-world.route.js
+++ routes/hello-world.route.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get("/api/home", (req, res) => {
-    res.send({message : "Hello World!"});
+    res.send({message : "Hello Dude!"});
 });
 
 export default router;
