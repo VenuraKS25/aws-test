84fc67935ac0762b835277ef3900b24ae2c84ce1
VenuraKS25 <vks250102@gmail.com>
2024-06-21 22:21:04 +0530

    adding socket.io and implementing websocket basics

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 import corsMiddleware from './middleware/cors.middleware.js';
 
-import {default as HelloRouter}  from './routes/hello-world.route.js';
+import { default as HelloRouter } from './routes/hello-world.route.js';
 
 dotenv.config();
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 app.use(corsMiddleware);
 
 app.use(HelloRouter);
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('triggerAlert', () => {
+        io.emit('showAlert');
+    });
+
+    // Handle client disconnect
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
